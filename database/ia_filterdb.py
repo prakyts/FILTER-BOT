f4eb8c867fcb67ee82b00500b9cd4519a6f21ade
prakyts <167399161+prakyts@users.noreply.github.com>
2025-06-17 11:53:25 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -23,7 +23,8 @@ sec_col = sec_db[COLLECTION_NAME]
 async def save_file(media):
     """Save file in the database."""
     
-    file_name = clean_file_name(media.caption) 
+    file_id = unpack_new_file_id(media.file_id)
+    file_name = clean_file_name(media.caption)
     
     file = {
         'file_id': file_id,
@@ -32,7 +33,7 @@ async def save_file(media):
         'caption': media.caption.html if media.caption else None
     }
 
-    if is_file_already_saved(file_name):
+    if is_file_already_saved(file_id, file_name):
         return False, 0
 
     try:
@@ -64,9 +65,10 @@ def clean_file_name(file_name):
         
     return ' '.join(filter(lambda x: not x.startswith('@') and not x.startswith('http') and not x.startswith('www.') and not x.startswith('t.me'), file_name.split()))
 
-def is_file_already_saved(file_name):
+def is_file_already_saved(file_id, file_name):
     """Check if the file is already saved in either collection."""
-    found = {'file_name': file_name}
+    found1 = {'file_name': file_name}
+    found = {'file_id': file_id}
 
     for collection in [col, sec_col]:
         if collection.find_one(found1) or collection.find_one(found):
